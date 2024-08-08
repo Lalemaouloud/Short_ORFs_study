a6585991f9f17459e5797aa811420fe68e8a450b
Lale MAOULOUD <127807581+Lalemaouloud@users.noreply.github.com>
2024-08-08 13:56:42 +0100

    Create combine_fastas.sh

---
+++ combine_fastas.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH -t 98:00:00               # Time limit 
+#SBATCH --mem=80G                # Memory 
+#SBATCH -o Run_out.txt            # Standard output file
+#SBATCH -e Run_err.txt            # Standard error file
+#SBATCH --mail-user=lale@ebi.ac.uk # Email for notifications
+#SBATCH --cpus-per-task=40        # Number of CPU cores
+#SBATCH --mail-type=BEGIN         # Email when the job starts
+#SBATCH --mail-type=END           # Email when the job ends
+
+
+#SLURM bash script that combine all .fasta files in the directory into one file named All_transcripts.fasta
+# to execute run : sbatch combine_fastas.sh
+
+#Author : LM
+
+
+
+cd /nfs/research/jlees/lale/small_ORFs/data/RNA-seq_data/SRA_FILES_Complete/sra_fasta_files
+
+# Define the output file
+output_file="All_transcripts.fasta"
+
+# Remove the output file if it already exists
+if [ -f "$output_file" ]; then
+    rm "$output_file"
+fi
+
+# Iterate over all .fasta files and append their content to the output file
+for file in *.fasta; do
+    if [ -f "$file" ]; then
+        cat "$file" >> "$output_file"
+        echo "" >> "$output_file"  # Add a newline for separation
+    fi
+done
+
+echo "All transcripts have been combined into $output_file"
